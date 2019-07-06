c722ee4a6da6bea733dbf9d1445dab9029834bdf
Sandeep Mansotra <sandeepsharma1998.sp@gmail.com>
2019-07-06 12:59:00 +0530

    rentomojo

---
+++ script.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "mydb"
+});
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/form.html'));
+});
+app.post('/submit',function(req,res){
+
+  var message=req.body.message;
+
+  res.write('Your comments submitted "' + req.body.message+'".\n');
+
+  con.connect(function(err) {
+  if (err) throw err;
+  var sql = "INSERT INTO comments (message) VALUES ('"+message+"')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+     res.end();
+  });
+  });
+})
+app.listen(3000);
+console.log("Running at Port 3000");
